09fdeaa1ad938c5f59906fbafe3d9da153d5bcd2
Oberd Nougbognonhou Kouassi <kouassioberd39@gmail.com>
2026-02-10 16:55:51 +0100

    Updated
     Updated the version of room and ksp to match with the current version of the kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
     buildFeatures {
         compose = true
     }
+
 }
 
 dependencies {
@@ -63,9 +64,12 @@ dependencies {
     implementation ("androidx.navigation:navigation-compose:2.7.7")
 
     // Room
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
+    //implementation("androidx.room:room-runtime:2.6.1")
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-ktx:$roomVersion")
+    implementation("androidx.room:room-runtime:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion") // Use ksp, not kapt
+    //ksp("androidx.room:room-compiler:2.6.1")
 
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
