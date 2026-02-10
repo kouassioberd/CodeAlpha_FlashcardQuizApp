f0813f46dd8cb635d60b60d8389f9800a54238ab
Oberd Nougbognonhou Kouassi <kouassioberd39@gmail.com>
2026-02-10 17:00:23 +0100

    NavGraph created
    To facilitate the navigation between screens

---
+++ app/src/main/java/com/example/codealpha_flashcardquizapp/navigation/NavGraph.kt
@@ -0,0 +1,31 @@
+package com.example.codealpha_flashcardquizapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.codealpha_flashcardquizapp.ui.screens.AddFlashcardScreen
+import com.example.codealpha_flashcardquizapp.ui.screens.FlashcardScreen
+import com.example.codealpha_flashcardquizapp.viewmodel.FlashcardViewModel
+
+
+@Composable
+fun NavGraph (modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    val viewModel: FlashcardViewModel = viewModel()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home",
+        modifier = modifier
+    ){
+        composable("home") {
+            FlashcardScreen(navController, viewModel)
+        }
+        composable("add") {
+            AddFlashcardScreen(navController, viewModel)
+        }
+    }
+}
