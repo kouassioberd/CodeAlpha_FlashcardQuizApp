7649ec37014940e0fb8e3c4eee6ffc0b004123c8
Oberd Nougbognonhou Kouassi <kouassioberd39@gmail.com>
2026-02-11 06:26:26 +0100

    Refactor(FlashcardScreen): make layout fully responsive and orientation-aware
    - Removed fixed height values
    - Added BoxWithConstraints for dynamic card sizing
    - Implemented landscape layout using Row
    - Added verticalScroll to prevent overflow
    - Centered content with max width constraint for tablets
    - Improved button layout using weight modifiers

--- app/src/main/java/com/example/codealpha_flashcardquizapp/ui/screens/FlashcardScreen.kt
+++ app/src/main/java/com/example/codealpha_flashcardquizapp/ui/screens/FlashcardScreen.kt
@@ -1,9 +1,11 @@
 package com.example.codealpha_flashcardquizapp.ui.screens
 
+import android.content.res.Configuration
 import android.graphics.Color
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -30,12 +34,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.codealpha_flashcardquizapp.data.entity.Flashcard
 import com.example.codealpha_flashcardquizapp.viewmodel.FlashcardViewModel
+
+
 
 
 @Composable
@@ -46,6 +54,9 @@ fun FlashcardScreen(
     val cards by viewModel.flashcards.collectAsState()
     var index by rememberSaveable { mutableStateOf(0) }
     var showAnswer by remember { mutableStateOf(false)}
+
+    val configuration = LocalConfiguration.current
+    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 
 
     Scaffold(
@@ -64,121 +75,224 @@ fun FlashcardScreen(
                 contentAlignment = Alignment.Center
             ){
                 Text(
-                    "No flashcard yet",
-                    fontSize = 20.sp,
+                    "No flashcard yet"
                 )
             }
         } else {
             val card = cards[index]
 
-            Column(
+           Box(
+               modifier = Modifier
+                   .fillMaxSize()
+                   .padding(padding),
+               contentAlignment = Alignment.Center
+           ) {
+
+               Column(
+                   modifier = Modifier
+                       .fillMaxWidth(0.9f)// prevents full stretch on tablet
+                       .fillMaxSize()
+                       .verticalScroll(rememberScrollState())
+                       .padding(24.dp),
+                   horizontalAlignment = Alignment.CenterHorizontally
+               ) {
+
+                   if (isLandscape) {
+                       Row(
+                           modifier = Modifier
+                               .fillMaxWidth(),
+                           horizontalArrangement = Arrangement.spacedBy(24.dp)
+                       ) {
+
+                           FlashcardContent(
+                               card = card,
+                               showAnswer = showAnswer,
+                               modifier = Modifier
+                                   .weight(1f)
+                           )
+
+                           FlashcardButtons(
+                               index = index,
+                               cardSize = cards.size,
+                               onPrevious = {
+                                   if (index > 0) {
+                                       index--
+                                       showAnswer = false
+                                   }
+                               },
+                               onNext = {
+                                   if (index < cards.size - 1) {
+                                       index++
+                                       showAnswer = false
+                                   }
+                               },
+                               onEdit = {
+                                   navController.navigate("edit/${card.id}")
+                               },
+                               onDelete = {
+                                   viewModel.deleteFlashcard(card)
+                                   if (index > 0) {
+                                       index--
+                                   }
+                               },
+                               showAnswer = showAnswer,
+                               onToggleAnswer = {showAnswer = !showAnswer},
+                               modifier = Modifier
+                                   .weight(1f)
+                           )
+                       }
+
+                   } else {
+
+                       FlashcardContent(
+                           card = card,
+                           showAnswer = showAnswer
+                       )
+                       Spacer(modifier = Modifier.height(24.dp))
+
+                       FlashcardButtons(
+                           index = index,
+                           cardSize = cards.size,
+                           onPrevious = {
+                               if (index > 0) {
+                                   index--
+                                   showAnswer = false
+                               }
+                           },
+                           onNext = {
+                               if (index < cards.size - 1) {
+                                   index++
+                                   showAnswer = false
+                               }
+                           },
+                           onEdit = {
+                               navController.navigate("edit/${card.id}")
+                           },
+                           onDelete = {
+                               viewModel.deleteFlashcard(card)
+                               if (index > 0) {
+                                   index--
+                               }
+                           },
+                           showAnswer = showAnswer,
+                           onToggleAnswer = {showAnswer = !showAnswer}
+                       )
+                   }
+               }
+           }
+        }
+    }
+}
+
+
+@Composable
+fun FlashcardContent(
+    card: Flashcard,
+    showAnswer: Boolean,
+    modifier: Modifier = Modifier
+) {
+    BoxWithConstraints(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        val dynamicHeigh = maxHeight * 0.5f
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(dynamicHeigh)
+                .shadow(8.dp, RoundedCornerShape(16.dp))
+                .background(
+                    MaterialTheme.colorScheme.surface,
+                    RoundedCornerShape(16.dp)
+                )
+                .padding(24.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = if (showAnswer) card.answer else card.question,
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+
+@Composable
+fun FlashcardButtons(
+    index: Int,
+    cardSize: Int,
+    onPrevious: () -> Unit,
+    onNext: () -> Unit,
+    onEdit: () -> Unit,
+    onDelete: () -> Unit,
+    showAnswer: Boolean,
+    onToggleAnswer: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Button(
+            onClick = onToggleAnswer,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                if (showAnswer) "Hide Answer" else "Show Answer"
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Button(
+                onClick = onPrevious,
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(24.dp)
-                    .padding(padding),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                    .weight(1f),
+                enabled = index > 0
             ) {
+                Text("Previous")
+            }
 
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(250.dp)
-                        .shadow(8.dp, RoundedCornerShape(16.dp))
-                        .background(MaterialTheme.colorScheme.surface, RoundedCornerShape(16.dp))
-                        .padding(24.dp),
-                    contentAlignment = Alignment.Center
-                ){
-                    Text(
-                        text = if (showAnswer) card.answer else card.question,
-                        style = MaterialTheme.typography.headlineSmall.copy(
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold
-                        ),
-                        color = MaterialTheme.colorScheme.onSurface
-                    )
-
-
-                }
-
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Button(
-                    onClick = { showAnswer = !showAnswer},
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = MaterialTheme.colorScheme.primary
-                    )
-
-                ) {
-                    Text( if (showAnswer) "Hide Answer" else "Show Answer")
-                }
-
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(16.dp)
-                ) {
-                    Button(
-                        onClick = {
-                            if (index > 0) {
-                                index--
-                                showAnswer = false
-                            }
-                        }
-                    ) {
-                        Text("Previous")
-                    }
-
-                    Button(
-                        onClick = {
-                            if (index < cards.size - 1) {
-                                index++
-                                showAnswer = false
-                            }
-                        }
-                    ) {
-                        Text("Next")
-                    }
-                }
-
-
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(16.dp)
-                ) {
-                    Button(
-                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
-                        onClick = {
-                            // Navigate to edit screen with the card ID as a parameter
-                            navController.navigate("edit/${card.id}")
-                        }
-                    ) {
-                        Text("Edit")
-                    }
-
-
-
-                    Spacer(modifier = Modifier.height(20.dp))
-
-                    Button(
-                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error),
-                        onClick = {
-                            viewModel.deleteFlashcard(card)
-                            if (index > 0) {
-                                index--
-                            }
-                        }
-                    ) {
-                        Text("Delete")
-                    }
-                }
-
-
-
+            Button(
+                onClick = onNext,
+                modifier = Modifier
+                    .weight(1f),
+                enabled = index < cardSize - 1
+            ) {
+                Text("Next")
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
 
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Button(
+                onClick = onEdit,
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text("Edit")
+            }
 
+            Button(
+                onClick = onDelete,
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text("Delete")
+            }
+        }
     }
 }
